06948782f1123362288eee450f7552f3a3dccced
sai135 <83128908+sai135@users.noreply.github.com>
2022-01-18 13:05:35 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,39 @@
+img = "";
+status1="";
+objectDetector="";
+objects=[];
+function preload(){
+  img = loadImage("tvac.jpg");
+}
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded() {
+  console.log("Model Loaded!");
+  status1=true;
+  objectDetector.detect(img, gotResult)
+}
+function gotResult(error, results) {
+  if (error) {
+ console.error(error);
+  }
+  console.log(results);
+  objects=results;
+}
+function draw() {
+  image(img, 0, 0, 640, 420);
+  if(status1 != "") {
+    for(i=0; i < objects.length; i++){
+      document.getElementById("status").innerHTML="Status = Object Detected";
+      document.getElementById("namer2").innerHTML="There are 1 big object in the image from which cocossd model has detected "+objects.length+" objects";
+    fill("#FF0000");
+    percent=floor(objects[i].confidence * 100)
+    text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+    noFill();
+    stroke("#FF0000");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height );
+    }}
+}
